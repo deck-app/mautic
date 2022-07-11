eda398e922a816eb94a30b3e3bbfeac4a6780dde
Naba Kumar Das <nabad600@gmail.com>
2022-07-11 18:34:00 +0530

    update sudo without password

--- mautic/login.sh
+++ mautic/login.sh
@@ -1,26 +1,40 @@
 #!/bin/sh
 cat << "EOF"
 
-                                                                                                                                                                                                               
-                                                                                                                                                                                                               
-`7MMF'     A     `7MF'`7MM"""YMM  `7MMF'        .g8"""bgd   .g8""8q. `7MMM.     ,MMF'`7MM"""YMM      `7MM"""Yb. `7MM"""YMM    .g8"""bgd `7MMF' `YMM'      .M"""bgd `7MM"""Mq.   db       .g8"""bgd `7MM"""YMM  
-  `MA     ,MA     ,V    MM    `7    MM        .dP'     `M .dP'    `YM. MMMb    dPMM    MM    `7        MM    `Yb. MM    `7  .dP'     `M   MM   .M'       ,MI    "Y   MM   `MM. ;MM:    .dP'     `M   MM    `7  
-   VM:   ,VVM:   ,V     MM   d      MM        dM'       ` dM'      `MM M YM   ,M MM    MM   d          MM     `Mb MM   d    dM'       `   MM .d"         `MMb.       MM   ,M9 ,V^MM.   dM'       `   MM   d    
-    MM.  M' MM.  M'     MMmmMM      MM        MM          MM        MM M  Mb  M' MM    MMmmMM          MM      MM MMmmMM    MM            MMMMM.           `YMMNq.   MMmmdM9 ,M  `MM   MM            MMmmMM    
-    `MM A'  `MM A'      MM   Y  ,   MM      , MM.         MM.      ,MP M  YM.P'  MM    MM   Y  ,       MM     ,MP MM   Y  , MM.           MM  VMA        .     `MM   MM      AbmmmqMA  MM.           MM   Y  , 
-     :MM;    :MM;       MM     ,M   MM     ,M `Mb.     ,' `Mb.    ,dP' M  `YM'   MM    MM     ,M       MM    ,dP' MM     ,M `Mb.     ,'   MM   `MM.      Mb     dM   MM     A'     VML `Mb.     ,'   MM     ,M 
-      VF      VF      .JMMmmmmMMM .JMMmmmmMMM   `"bmmmd'    `"bmmd"' .JML. `'  .JMML..JMMmmmmMMM     .JMMmmmdP' .JMMmmmmMMM   `"bmmmd'  .JMML.   MMb.    P"Ybmmd"  .JMML. .AMA.   .AMMA. `"bmmmd'  .JMMmmmmMMM 
-                                                                                                                                                                                                               
-                                                                                                                                                                                                               
-                                                                                                                                                                                                               
-                                                                                                                                                       ,,                                                      
-                                                                                                            `7MMM.     ,MMF'                    mm     db               .M"""bgd mm                  `7MM      
-                                                                                                              MMMb    dPMM                      MM                     ,MI    "Y MM                    MM      
-                                                                                                              M YM   ,M MM   ,6"Yb.`7MM  `7MM mmMMmm `7MM  ,p6"bo      `MMb.   mmMMmm  ,6"Yb.  ,p6"bo  MM  ,MP'
-                                                                                                              M  Mb  M' MM  8)   MM  MM    MM   MM     MM 6M'  OO        `YMMNq. MM   8)   MM 6M'  OO  MM ;Y   
-                                                                                                              M  YM.P'  MM   ,pm9MM  MM    MM   MM     MM 8M           .     `MM MM    ,pm9MM 8M       MM;Mm   
-                                                                                                              M  `YM'   MM  8M   MM  MM    MM   MM     MM YM.    ,     Mb     dM MM   8M   MM YM.    , MM `Mb. 
-                                                                                                            .JML. `'  .JMML.`Moo9^Yo.`Mbod"YML. `Mbmo.JMML.YMbmd'      P"Ybmmd"  `Mbmo`Moo9^Yo.YMbmd'.JMML. YA
+       !
+       ^
+      / \
+     /___\
+    |=   =|
+    |     |
+    |  D  |
+    |     |
+    |  E  |
+    |     |
+    |  C  |
+    |     |
+    |  K  |
+    |     |
+   /|##!##|\
+  / |##!##| \
+ /  |##!##|  \
+|  / ^ | ^ \  |
+| /  ( | )  \ |
+|/   ( | )   \|
+    ((   ))
+   ((  :  ))
+   ((  :  ))
+    ((   ))
+     (( ))
+      ( )
+       .
+       .
+       .
+
+8b    d8    db    88   88 888888 88  dP""b8     .dP"Y8 888888    db     dP""b8 88  dP 
+88b  d88   dPYb   88   88   88   88 dP   `"     `Ybo."   88     dPYb   dP   `" 88odP  
+88YbdP88  dP__Yb  Y8   8P   88   88 Yb          o.`Y8b   88    dP__Yb  Yb      88"Yb  
+88 YY 88 dP""""Yb `YbodP'   88   88  YboodP     8bodP'   88   dP""""Yb  YboodP 88  Yb 
 
 EOF
 sudo -u {USER} /bin/ash
